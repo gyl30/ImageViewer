959889547f6a1afeb9da00a4e3db6bae532a4639
gyl30 <im.ggyyll@gmail.com>
2026-05-19 15:06:09 +0800

    feat(scanner): 添加文件名自然排序

--- file_scanner.cc
+++ file_scanner.cc
@@ -1,9 +1,20 @@
 #include "file_scanner.h"
+#include <algorithm>
+#include <QCollator>
 #include <QDirIterator>
 #include <QImageReader>
 #include <QElapsedTimer>
 #include <QFileInfo>
 #include <QDebug>
+
+namespace
+{
+struct scanned_image
+{
+    image_meta meta;
+    QString file_name;
+};
+}
 
 file_scanner::file_scanner(QObject* parent) : QObject(parent), stop_flag_(false) {}
 
@@ -19,7 +30,7 @@ void file_scanner::start_scan(const QString& dir_path, int session_id)
     filters << "*.jpg" << "*.jpeg" << "*.png" << "*.bmp" << "*.gif" << "*.webp";
 
     QDirIterator it(dir_path, filters, QDir::Files, QDirIterator::Subdirectories);
-    QList<image_meta> scanned_images;
+    QList<scanned_image> scanned_images;
     int total_count = 0;
 
     while (it.hasNext())
@@ -38,13 +49,25 @@ void file_scanner::start_scan(const QString& dir_path, int session_id)
             continue;
         }
 
-        image_meta meta;
-        meta.path = file_path;
-        meta.original_size = size;
-
-        scanned_images.append(meta);
+        scanned_images.append({{file_path, size}, QFileInfo(file_path).fileName()});
         total_count++;
     }
+
+    QCollator collator;
+    collator.setNumericMode(true);
+    collator.setCaseSensitivity(Qt::CaseInsensitive);
+
+    std::sort(scanned_images.begin(),
+              scanned_images.end(),
+              [&collator](const scanned_image& left, const scanned_image& right)
+              {
+                  int cmp = collator.compare(left.file_name, right.file_name);
+                  if (cmp != 0)
+                  {
+                      return cmp < 0;
+                  }
+                  return left.meta.path < right.meta.path;
+              });
 
     const qsizetype batch_size = 100;
     for (qsizetype i = 0; i < scanned_images.size(); i += batch_size)
@@ -54,7 +77,7 @@ void file_scanner::start_scan(const QString& dir_path, int session_id)
         qsizetype end = std::min(i + batch_size, scanned_images.size());
         for (qsizetype j = i; j < end; ++j)
         {
-            batch.append(scanned_images[j]);
+            batch.append(scanned_images[j].meta);
         }
         emit images_scanned_batch(batch, session_id);
     }
